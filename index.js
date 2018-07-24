d795d0397de7ab9c7c598284ffd53da5e52228e0
Shelby Trinca <shelby.trinca@gmail.com>
2018-07-24 18:15:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  greeting = `Happy holidays, ${name}!`
+  return greeting
+}
